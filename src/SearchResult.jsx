a6e3cecc50c0d9f9333b46dff1b25b3c512103fc
Shawna <shawna@appacademy.io>
2017-12-11 23:23:22 -0500

    Open links in new tab

--- src/SearchResult.jsx
+++ src/SearchResult.jsx
@@ -9,7 +9,7 @@ export default class SearchResult extends Component {
     const { result } = this.props
     return <div className="gem">
       <h5 data-test="name" className="gemName">{result.name}</h5>
-      <a href={result.project_uri} className="link">view dependencies</a>
+      <a href={result.project_uri} className="link" target="__blank">view dependencies</a>
       <p data-test="info">{result.info}</p>
       <button onClick={this.handleFavorite} className="btn btn-outline-primary">Favorite</button>
     </div>
